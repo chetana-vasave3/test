49c816a3b4da373748cede1fef523908b09ca95a
chetana-vasave3 <chetanavasave3@gmail.com>
2025-03-24 12:15:04 +0530

    adding new project

---
+++ Assignment set 3.sql
@@ -0,0 +1,16 @@
+#use customers table 
+-- Q1. List customers who do not have a sales representative assigned.
+-- Q2. Increase the credit limit of all customers by 10%.
+
+#use payments table 
+-- Q3.What is the total amount for customerno 103?
+-- Q4.How many entries belong to the customerno 141?
+-- Q5.Find the minimum and maximum amounts in the dataset.movies
+
+#Use movies table
+-- Q6.What are the titles of all movies in the "Animations" category?
+-- Q7.How many movies belong to the "Action" category?
+-- Q8.Write an SQL query to update the category of "Marley and Me" from "Romance" to "Drama".
+-- Q9.Retrieve all movies that belong to the "Animations" category AND have an ID greater than 2.
+-- Q10.Retrieve all movies where the category is "Action" OR "18+".
+	
